c5b4fa01f29f1edf079b457ed801aa68031414a4
Amit Kumar <kumarboy427@gmail.com>
2024-11-04 19:02:38 +0530

    Complete Server Request

--- src/app.js
+++ src/app.js
@@ -10,8 +10,19 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter);
+console.log(app);
+
 export default app;
